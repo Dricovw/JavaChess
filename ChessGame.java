aeda6bdc9d29de67332591905ed546762a7a9cbd
Dricovw <112882481+Dricovw@users.noreply.github.com>
2024-01-13 15:17:10 +0100

    javac

---
+++ ChessGame.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+    public class ChessGame {
+        private char[][] chessboard;
+
+        public ChessGame() {
+            // Initialize the chessboard
+            chessboard = new char[][] {
+                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+                    {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+            };
+        }
+
+        public void start() {
+            displayChessboard();
+        }
+
+        private void displayChessboard() {
+            System.out.println("  a b c d e f g h");
+            System.out.println(" +----------------");
+            for (int i = 0; i < 8; i++) {
+                System.out.print(8 - i + "| ");
+                for (int j = 0; j < 8; j++) {
+                    System.out.print(chessboard[i][j] + " ");
+                }
+                System.out.println("| " + (8 - i));
+            }
+            System.out.println(" +----------------");
+            System.out.println("  a b c d e f g h");
+        }
+    }
---
+++ Main.java
@@ -0,0 +1,11 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        ChessGame chessGame = new ChessGame();
+        chessGame.start();
+    }
+}
+
+// ChessGame.java
+
+
