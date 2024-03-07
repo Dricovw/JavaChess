3322ae1494b5bdfa5911e20a2dd386b27919d4ee
Tangobyte <112774024+Tangobyte@users.noreply.github.com>
2024-03-07 02:32:51 +0100

    factory logic

--- pieces/PieceFactory.java
+++ pieces/PieceFactory.java
@@ -1,11 +1,27 @@
 package pieces;
 
-public class PieceFactory {
-    public static ChessPiece createPawn(char symbol) {return new Pawn(symbol);}
-    public static ChessPiece createRook(char symbol) {return new Rook(symbol);}
+public interface PieceFactory {
+    static ChessPiece createPawn(char symbol) {
+        return null;
+    }
 
-    public static ChessPiece createKnight(char symbol) {return new Knight(symbol);}
-    public static ChessPiece createBishop(char symbol) {return new Bishop(symbol);}
-    public static ChessPiece createKing(char symbol) {return new King(symbol);}
-    public static ChessPiece createQueen(char symbol) {return new Queen(symbol);}
+    static ChessPiece createRook(char symbol) {
+        return null;
+    }
+
+    static ChessPiece createKnight(char symbol) {
+        return null;
+    }
+
+    static ChessPiece createBishop(char symbol) {
+        return null;
+    }
+
+    static ChessPiece createKing(char symbol) {
+        return null;
+    }
+
+    static ChessPiece createQueen(char symbol) {
+        return null;
+    }
 }
